92a019c01042e609df00507de521141ab5170616
afifahzhraa <afifahzahra134@gmail.com>
2025-12-18 18:32:25 +0700

    Initial commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Diabetes Classification")
+
+df = pd.read_csv("diabetes_preprocessing/diabetes_clean.csv")
+
+X = df.drop(columns=["Outcome"])
+y = df["Outcome"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    mlflow.sklearn.autolog()
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=None,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    accuracy = model.score(X_test, y_test)
